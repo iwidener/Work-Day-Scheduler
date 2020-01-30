4eb5367b4edf4c8fd563b500d0d6dcf54a52f5bf
Inna Widener <iwide2019@gmail.com>
2020-01-29 23:37:56 -0600

    Codes added. Readme.md updated.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Work-Day-Scheduler
+This is a daily planner for the people who like to plan and schedule their day. 
+
+Here is a description of this planner:
+the current day is displayed at the top of the calendar;
+the timeblocks are for standard business hours (from 9:00 AM to 5:00 PM);
+each timeblock is color coded to indicate whether it is in the past, present ot future;
+an event can be entered into a timeblock;
+when the save button for that timeblock is clicked, an event is saved in local storage;
+when the page is refreshed, the saved events persist.
--- script.js
+++ script.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
 
     console.log("ready");
 
+    //Current Date
+
+    $("#currentDay").text(curDay);
+
+    //Creating div, classes and variables
+
     var blockDiv = $(".time-block");
 
     var hour = 9;
@@ -31,22 +37,60 @@ $(document).ready(function () {
         newRow.append(hourColumn, descriptionColumn, saveBtn);
         saveBtn.append(iconBtn);
         blockDiv.append(newRow);
+        descriptionColumn.append(presentColor, pastColor, futureColor);
         hour++;
 
         if (hour === 12) {
-            dateTime = "PM"
+            dateTime = "PM";
         }
 
         else if (hour === 13) {
-            hour = 1
+            hour = 1;
+        }
+
+        //Assigning the colors
+
+        var presentColor = $("<div>");
+        presentColor.attr("class", "present");
+
+        var pastColor = $("<div>");
+        pastColor.attr("class", "past");
+
+        var futureColor = $("<div>");
+        futureColor.attr("class", "future");
+
+        if (hourColumn === m) {
+            descriptionColumn = "present";
+        } else if (hourColumn < m) {
+            descriptionColumn = "past";
+        } else {
+            descriptionColumn = "future";
         }
 
     }
 
+    //Local Storage
 
+    var events = JSON.parse(localStorage.getItem("events")) || {};
 
+    $.each(events, function (key, value) {
+        $("[data-hour=" + "]").children("col-8 description").val(value)
+    })
+
+    $(document).on("click", ".fas", "col-2 saveBtn", function () {
+        event.preventDefault();
+
+        var userInput = $(this).siblings("col-8 description").val();
+
+        var hour = $(this).parent().attr("data-hour");
+
+        events[hour] = userInput
+
+        localStorage.setItem("events", JSON.stringify(events));
+
+    })
 });
 
 
 
-    
+
